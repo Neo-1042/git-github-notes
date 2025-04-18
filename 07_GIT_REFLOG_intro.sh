1bb05172e335c4b370886cb1236cd4da51a76cc5
Neo-1042 <rafaelh.71642@gmail.com>
2025-04-18 02:07:01 -0600

    Finishes GIT REFLOG file

--- 07_GIT_REFLOG_intro.sh
+++ 07_GIT_REFLOG_intro.sh
@@ -10,3 +10,22 @@ git reset --hard HEAD~1 # Delete everything everywhere
 git reflog
 # Stores ALL the changes for 30 days
 # Challenge: delete this changes and THEN get them back
+git reset --hard 431c7f1 # Bringing back the lost commit
+# SUCCESS
+##################################################
+# DELETED BRANCHES and GIT REFLOG
+git switch -c featureX # Create and switch to a new branch
+touch DSCredentials.java
+git add DSCredentials.java
+git commit -m "Adds datasource Credencials java file"
+git branch master
+git branch -D featureX # Oh NO!!! We deleted a branch that we actually needed
+# How can I get my branch back? :c
+git reflog # featureXhash10
+# Recreate the branch in 2 steps:
+# 1
+git checkout featureXhash10 # DETACHED HEAD mode
+# 2
+git switch -c featureX
+# This last step associates the detached HEAD to the new branch :D
+##################################################
