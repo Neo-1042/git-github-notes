3e488d08d9016d37c3e72ed84a2212646e421ac2
Neo-1042 <rafaelh.71642@gmail.com>
2025-04-18 01:46:59 -0600

    Agrega git reflog

---
+++ 07_GIT_REFLOG_intro.sh
@@ -0,0 +1,11 @@
+################### GIT REFLOG ################### 
+# git log      => Commits area
+# git ls-files => Staging Area
+# Say you delete the last commit, like this:
+git reset --soft HEAD~1 # Keep the changes on the STAGING AREA
+git reset HEAD~1 # Default: delete the commit and delete the changes from the SA, but keeps it on the local files
+git reset --hard HEAD~1 # Delete everything everywhere
+# What if we need back the deleted information??
+##################################################
+
+
