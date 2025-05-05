fdca2deac82a0a4ad7e6ed026181abc6f9aac767
Neo-1042 <rafaelh.71642@gmail.com>
2025-05-04 22:20:37 -0600

    Agrega GITHUB_INTRO shell. Prepara para GitHub

---
+++ 13_GITHUB_INTRO.sh
@@ -0,0 +1,43 @@
+################################ GITHUB_INTRO ################################
+# What is GitHub?
+# How are git and GitHub connected?
+# Remote branches, remote tracking branches, local tracking branches
+# Understanding upstreams
+# git clone
+###############################################################################
+# GitHub = Remote git repository hosting
+# Connecting git with GitHub :D
+# 1] Local git repo to empty remote repo
+# git ------------> GitHub = origin (alias/shorthand of the URL of the remote repo)
+
+# Establish a connection between local and remote
+git remote add origin URL # origin := URL
+git remote add ssh-origin git@github.com:x/myrepo.git
+# Push the local data from git to GitHub
+git push
+git push origin master
+# Pull the remote data from GitHub to git
+git pull
+
+#### pull, fetch and merge in depth
+# Reminder: git pull = fetch + merge (or rebase)
+git fetch # updates changes from GitHub to my local git (NOT yet to my WD)
+# Updates the remote-tracking branches under
+    # /refs/remotes/<remoteRepoName>/
+# This operation is safe to run at any time, since it never changes any of your local branches (WD)
+# under
+    # /refs/heads/
+git pull # pulls code from GitHub to my working directory.
+# git pull brings a local branch up-to-date with its remote version, while also updating your other
+# remote-tracking branches
+
+################### CLONING ############################
+# GitHub ---------------------------> 
+git clone https://github.com/q2e213xxx
+# You can clone via HTTPs (Server certificates, TLS) or SSH (more secure)
+# Recommended order:
+git add .
+git commit -m "Cambia xml"
+git push -u origin development # Upstream
+git tag -a v1.0.0 -m "First stable release wujuu"
+
--- branches1.sh
+++
@@ -1,4 +0,0 @@
-# The master branch holds all the commits made there
-# branch 1 will contain all previous commits, but will work independently from mb
-# 
-# Master branch
