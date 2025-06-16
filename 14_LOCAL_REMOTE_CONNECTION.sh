e226fb52cadc0eedfc3aa6b3f8b6b14741829d4a
Neo-1042 <rafaelh.71642@gmail.com>
2025-06-16 00:13:32 -0600

    Documentation

--- 14_LOCAL_REMOTE_CONNECTION.sh
+++ 14_LOCAL_REMOTE_CONNECTION.sh
@@ -5,8 +5,29 @@ git remote add origin ${URL}
 git remote -v # Verify that you linked the local <-> remote repos correctly
 #######################################################################
 # Understading the workflow
+
+# Local branch             -> master
+# Remote tracking branch   -> remotes/origin/master (intermediary)
+# Remote branch ("origin") -> master
+
+git fetch # fetch the latest changes from the remote branch and add them to remotes/origin/master
+git merge # remotes/origin/master (local, read-only for us)
+# fetch + merge = pull
+#######################################################################
 git branch -a # Display remote and local branches
 # remotes/origin/master
 git branch -r # Display only remote branches
 git branch -vv # Verbose
 
+git branch feature # local
+git add .
+git commit -m "Add bla bla"
+git push origin feature
+#################################
+git branch -a
+# * feature
+#   master
+#   remotes/origin/feature
+#   remotes/origin/master
+
+git ls-remote # list all the ACTUAL remote branches
