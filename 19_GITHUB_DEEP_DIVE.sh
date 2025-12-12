65baf6f2b8f04097a4217b2c4ae1be25a2bd2340
Neo-1042 <rafaelh.71642@gmail.com>
2025-12-11 19:56:41 -0600

    GitHub Deep Dive. GitHub security management

---
+++ 19_GITHUB_DEEP_DIVE.sh
@@ -0,0 +1,37 @@
+# Git-Hub --- Collaboration and Contribution
+
+# Module content
+# Understanding GitHub Accounts and Repository Types
+# Collaborating in GitHub and Open Source Projects
+# Creating your GitHub Portfolio Page
+# ====================================================================================== #
+# 4 GitHub Main Usecases
+
+# 1] Cloud Storage for a single user.
+# 2] Portfolio Page for a single user.
+# 3] Collaboration (part of the actual project)
+# 4] Contribution (forking projects, fix bugs and ask for permission to make changes)
+
+# Account types: Personal User, Organizational and Enterprise
+# Enterprise -> Central management of multiple GitHub accounts. 
+# GitHub Enterprise Cloud & GitHub Enterprise Server.
+
+# Changing the repository type from PUBLIC to PRIVATE
+# ====================================================================================== #
+# Publish Commits to a Public Repository
+
+# Even if you have access to the public repository to "git clone" it, you still cannot
+# publish commits without having a means of identification to GitHub as the owner of that
+# remote repository.
+# ====================================================================================== #
+# How does GitHub manage Security?
+
+# At least, we need the personal access token (or SSH key-pair) to be able to make changes
+# in a public repository.
+
+# GitHub Accounts:
+# Personal User Account -> Owner Access || Collaborator Access
+
+# GitHub > repository > Settings > Manage Access > Who has access? > Direct Access section
+# Manage Access : Invite a collaborator
+# Pending invite (...)
