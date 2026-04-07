e3487aeb1cf1388e041b53c626694cae8453cd2d
Neo-1042 <rafaelh.71642@gmail.com>
2026-04-06 23:58:48 -0600

    Refresh on collaboration and contribution

---
+++ 22_COLLABORATION.sh
@@ -0,0 +1,15 @@
+# MODULE CONTENT #
+# Understanding GitHub Accounts and Repository Types
+# Collaborating in GitHub and Open Source Projects
+# Creating your own GitHub Portfolio Page
+#==================================================================================================#
+# GitHub Use Cases
+# 1] Cloud Storage (Single User)
+# 2] Portfolio Page (Single User)
+# 3] Collaboration
+# 4] Contribution
+
+# Account Types
+# Personal User Account
+# Organizational Account
+# Enterprise Account
