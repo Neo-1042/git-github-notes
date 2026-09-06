2af71abe409196b8ee1acba4bdb7c1d6b3ce2034
Neo-1042 <rafaelh.71642@gmail.com>
2026-09-05 23:58:02 -0600

    Collect git starting notes

--- 00_git_intro.sh
+++ 00_git_intro.sh
@@ -37,3 +37,85 @@ git checkout other-branch
 git checkout -b new-branch
 git switch -c new-branch
 git merge other-branch
+
+
+#### git Introduction #####
+# Basic Commands
+
+git init
+git add <file>
+git status
+git commit
+git push
+git pull
+git clone
+
+# WORKING DIRECTORY
+git add
+# STAGING AREA
+git commit
+# REPOSITORY
+
+# git bash available (for Windows, for example)
+
+cd /Users/Rafael/Desktop/Project
+git init # .git is created
+
+git status
+# Add files to staging area
+git add index.html 
+git add myApp.js 
+git status
+
+git commit # Who are you???
+git config --global user.email "rafael.dhr@ciencias.unam.mx"
+git config --global user.name "Batman"
+
+git commit -m "Added 3 files etc." # A hash is created
+git log # Returns the hash (identifier)
+
+# Sistema de control de versiones
+# Recuperar versiones especificas
+# Comparar cambios a lo largo del tiempo
+################## Git and GitHub. Academind ##################
+# git is a local tool. That's why we need GitHub
+# Cloud hosting and collaboration provider
+# Let's start with the Z shell (zsh)
+# More possibilities: start servers, 
+#                     install tools,
+#                     run code,
+#                     execute files,
+#                     work with git.
+# Windows offers 3 options: command prompt (cmd), PowerShell (W7), Git Bash (emulation)
+~   # Home directory
+# Absolute vs Relative Paths
+/   # Root directory
+cd .. # go back one layer in directories
+#################### Creating and deleting files
+mkdir 
+rmdir folder1 # only empty folders
+touch # "touch" existing files and change its state (or create a new one)
+touch style.css script.js
+rm style.css script.js # gone forever
+
+##################### Flags
+cd practice
+touch index.html style1.css
+ls -s # size
+ls -l # long format
+ls -ls # long format + size
+man ls # manual
+man rm
+rm -r folder2 # or -R. Removes the folder even if it is non-empty
+
+####################### Copying and Moving Files
+mkdir copied
+mkdir moved
+touch file1.html file2.js
+cd ..
+# cp source target
+cp data/index.html copied/
+cd copied
+cp -r data/ copied/ # -r = recursive. Copies all the contents from data/ into copied/
+mv data/index.html moved/
+###################################################################################
