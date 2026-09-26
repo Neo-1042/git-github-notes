6de41e812687ab21d0401c9c17d2f8801ac8a6ae
Neo-1042 <rafaelh.71642@gmail.com>
2026-09-25 23:45:49 -0600

    React Meals Project init

---
+++ ReactMealsProject/00_git_github_project_tech_notes.sh
@@ -0,0 +1,8 @@
+###################################################################################################
+# React + git + GitHub Interface-Only Project
+# REACTMEALS
+# Choose meals and add them to your cart. Manage your cart
+# NodeJS --> React
+# These notes focus on git + GitHub
+###################################################################################################
+# 20260925 -> Review (Circel + Ame). Stay focused
